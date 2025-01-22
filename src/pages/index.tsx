4d84db7edd79184cf2dbbb1cb563789d40dd7581
marquessam <samuelnmarques@gmail.com>
2025-01-22 18:51:56 +0800

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Leaderboard from '@/components/Leaderboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-950 py-8">
+      <Leaderboard />
+    </main>
+  );
+}
