e6e37b1aa5a156049bd8a2751feb73dbd1ad2e68
marquessam <samuelnmarques@gmail.com>
2025-01-22 18:48:43 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['retroachievements.org', 'i.ibb.co'],
+  },
+}
+
+module.exports = nextConfig
