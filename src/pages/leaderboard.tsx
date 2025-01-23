c2f2aa9cf1d66a4e09d61c68df3b211118bfc1ca
marquessam <samuelnmarques@gmail.com>
2025-01-23 15:16:23 +0800

    Create leaderboard.tsx

---
+++ src/pages/leaderboard.tsx
@@ -0,0 +1,9 @@
+import Leaderboard from '@/components/Leaderboard';
+
+export default function LeaderboardPage() {
+  return (
+    <div className="bg-[#17254A] min-h-screen">
+      <Leaderboard />
+    </div>
+  );
+}
